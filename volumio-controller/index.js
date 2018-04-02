5fddea0b6aea0e73e7b18b8d059f64e952e99b22
Lars Arvidsson <lars.arvidsson@gmail.com>
2018-04-02 13:25:32 +0200

    Using websocket api for volumio

--- volumio-controller/index.js
+++ volumio-controller/index.js
@@ -1,18 +1,29 @@
 const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
-const http = require("http");
+var io = require("socket.io-client");
 
-const controls = require('./controls.json');
-const albums = require('./albums.json');
+const controls = require("./controls.json");
+const music = require("./music.json");
 
 const serialPort = process.env.SERIAL_PORT || "/dev/ttyACM0"; // "/dev/tty.usbmodem1431" on my Mac
 const volumioServer = process.env.VOLUMIO_SERVER || "http://localhost:3000"; // http://volumio.local
+
+var socket = io.connect(volumioServer);
+
+socket.on("pushState", function(data) {
+  console.log("status: " + data.status);
+  console.log(data.artist + " - " + data.album + " - " + data.title);
+});
+
+socket.on("disconnect", function() {
+  console.log("disconnected");
+});
 
 const port = new SerialPort(serialPort, {
   baudRate: 9600
 });
 
-port.on('error', function(error) {
+port.on("error", function(error) {
   console.log("error: " + error.message);
 });
 
@@ -21,26 +32,40 @@ const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
 parser.on("data", data => {
   console.log(data);
 
-  if (controls.play === data) {
-    console.log("play");
-    http.get(volumioServer + "/api/v1/commands/?cmd=play");
-  } else if (controls.pause === data) {
-    console.log("pause");
-    http.get(volumioServer + "/api/v1/commands/?cmd=pause");
-  } else if (controls.next === data) {
-    console.log("next");
-    http.get(volumioServer + "/api/v1/commands/?cmd=next");
-  } else if (controls.previous === data) {
-    console.log("previous");
-    http.get(volumioServer + "/api/v1/commands/?cmd=prev");
-  } else {
-    const album = albums[data];  
-    if (album) {
-      console.log("album: " +  album);
-      var albumVar = encodeURIComponent(album);
-      var url = volumioServer + "/api/v1/commands/?cmd=playplaylist&name=" + album;
-      http.get(url);      
+  const controlItem = controls[data];
+  const musicItem = music[data];
+
+  if (controlItem) {
+    console.log(controlItem);
+    socket.emit(controlItem);
+  } else if (musicItem) {
+    console.log(musicItem);
+
+    if (musicItem.album) {
+      const encodedArtist = encodeURIComponent(
+        musicItem.artist ? musicItem.artist : ""
+      );
+      const encodedAlbum = encodeURIComponent(musicItem.album);
+      const uri = "albums://" + encodedArtist + "/" + encodedAlbum;
+      console.log(uri);
+      socket.emit("replaceAndPlay", { uri: uri });
     }
   }
 });
 
+if (process.platform === "win32") {
+  var rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on("SIGINT", function() {
+    process.emit("SIGINT");
+  });
+}
+
+process.on("SIGINT", function() {
+  console.log("Caught interrupt signal");
+  socket.disconnect();
+  process.exit();
+});
