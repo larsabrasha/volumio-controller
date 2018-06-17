67b3e6310d52b362245566c1f127b55edbf37d46
Lars Arvidsson <lars.arvidsson@gmail.com>
2018-06-17 15:27:08 +0200

    Fix for duration in queue

--- musicplayer/src/app/minute-seconds.pipe.ts
+++ musicplayer/src/app/minute-seconds.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MinuteSecondsPipe implements PipeTransform {
   transform(value: number): string {
-    const minutes = Math.floor(value / 60);
-    const seconds = value - minutes * 60;
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    if (value != null) {
+      const minutes = Math.floor(value / 60);
+      const seconds = value - minutes * 60;
+      return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    }
+    return '';
   }
 }
